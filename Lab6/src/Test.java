737b2a4f774c22d9ac118ad358d9e47f382dd406
MariaFedorova <57068851+MariaFedorova@users.noreply.github.com>
2019-12-12 23:21:39 +0300

    Add files via upload

---
+++ Lab6/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        Car c1 = new Car("BMW", "F3x", "Long", 350000, 2007);
+        Car c2 = new Car("Mercedes-Benz", "W205", "Original", 2150000, 2019);
+        Car c3 = new Car("Chevrolet", "Camaro", "Cross", 2760000, 2017);
+        c1.println();
+        c2.println();
+        c3.println();
+    }
+}
+
